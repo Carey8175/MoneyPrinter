f753459fb967c14caeac1e0197956095052fe3e8
carey <980328708@qq.com>
2025-02-09 06:54:08 +0800

    signal_candle_info 类型改为datafram

--- system_code/core/modules/position.py
+++ system_code/core/modules/position.py
@@ -11,7 +11,7 @@ class HoldingPeriod:
             side: str,
             leverage: float = 1.0,
             fee_rate: float = 0.0005,
-            signal_candle_info: list = None
+            signal_candle_info: pd.DataFrame = None
     ):
         """
         初始化持仓类
@@ -31,7 +31,7 @@ class HoldingPeriod:
         self.fee_rate = fee_rate  # 手续费费率（可以在平仓时计算）
         self.more_info = {}     # 用于存储额外信息
         # 用于存储信号出现时的K线信息, 目前的主要用途是做聚类，cols和data的特征一致
-        self.signal_candle_info = [] if signal_candle_info else self.data[0].values.tolist()
+        self.signal_candle_info = signal_candle_info if signal_candle_info else self.data.iloc[0]
 
         # 计算时初始化
         self.final_profit = None  # 最终收益（平仓后计算）
